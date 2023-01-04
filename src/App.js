730b18fcfa908511395776bdadd1c1b4e09a9eb9
Peter <peterhansendev.com>
2023-01-04 11:16:33 +0100

    mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [colorData, setColorData] = useState([]);
+  const [colorPicker, setColorPicker] = useState([]);
+  const [colorMode, setColorMode] = useState([]);
+  const [before, setBefore] = useState(true);
+
+  //console.log(colorData);
+  console.log(colorPicker);
+  console.log(colorMode);
+
+  function colorSearch() {
+    fetch(
+      `https://www.thecolorapi.com/scheme?hex=${colorPicker}&mode=${colorMode}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setColorData(data.colors);
+      });
+    setBefore(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className={before ? "topBefore" : "top"}>
+        <input
+          type="color"
+          onChange={(e) => setColorPicker(e.target.value.slice(1))}
+        />
+        <select onChange={(e) => setColorMode(e.target.value)}>
+          <option value="monochrome">Monochrome</option>
+          <option value="monochrome-dark">monochrome-dark</option>
+          <option value="monochrome-light">monochrome-light</option>
+          <option value="analogic">analogic</option>
+          <option value="complement">complement</option>
+          <option value="analogic-complement">analogic-complement</option>
+          <option value="triad">triad</option>
+          <option value="quad">quad</option>
+        </select>
+        <button
+          disabled={colorPicker.length && colorMode.length > 0 ? false : true}
+          onClick={colorSearch}
         >
-          Learn React
-        </a>
-      </header>
+          Get Color Scheme
+        </button>
+      </div>
+      <div className="display">
+        {colorData.map((color) => (
+          <div key={color.hex.value}>
+            <div
+              className="display-colors"
+              style={{ backgroundColor: color.hex.value }}
+            ></div>
+            <p>{color.hex.value}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+/** disable button */
